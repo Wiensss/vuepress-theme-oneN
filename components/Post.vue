cc5b50eb0217adb766ef6e0e2b2f40f178990180
Vincent Hy <Veminem@outlook.com>
2019-08-08 07:12:44 +0800

    theme layout in realization

---
+++ components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Content />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Post'
+}
+</script>
+
+<style>
+</style>
---
+++ layouts/Layout.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-container>
+    <el-header>Nav</el-header>
+
+    <el-container>
+      <Menu />
+      <Post />
+    </el-container>
+
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+import Menu from '@theme/components/Menu'
+import Post from '@theme/components/Post'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    Menu,
+    Post
+  }
+}
+</script>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
