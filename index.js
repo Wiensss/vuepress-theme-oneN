17158c9cf31943662a2796fab81975b58c5197d1
Vincent Hy <Veminem@outlook.com>
2019-10-13 10:46:22 +0800

    add medium-zoom plugin

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ module.exports = (options, ctx) => ({
     ['@vuepress/search'],
     ['@vuepress/plugin-nprogress'],
     [
+      '@vuepress/medium-zoom',
+      { selector: '.content__default :not(a) > img' }
+    ],
+    [
       'container',
       {
         type: 'tip',
-        defaultTitle: { '/zh/': '提示'}
+        defaultTitle: { '/zh/': '提示' }
       }
     ],
     [
